d9875f65c7450ba1494dd6dc3be24bfa07484a5e
Kiyoshi Woolheater <my email address used on my git repo>
2017-04-04 12:19:11 -0400

    Finish getting sessionID and then logging in to mapViewController

--- OnTheMap/AppDelegate.swift
+++ OnTheMap/AppDelegate.swift
@@ -12,7 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var sessionID: String? = "123"
+    var userID: Int? = nil
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LoginViewController: UIViewController {
     
     var keyboardOnScreen = false
+    var appDelegate: AppDelegate!
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
@@ -20,13 +21,85 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
         subscribeToNotification(.UIKeyboardWillShow, selector: #selector(keyboardWillShow))
         subscribeToNotification(.UIKeyboardWillHide, selector: #selector(keyboardWillHide))
         subscribeToNotification(.UIKeyboardDidShow, selector: #selector(keyboardDidShow))
         subscribeToNotification(.UIKeyboardDidHide, selector: #selector(keyboardDidHide))
     }
     
+    func performUIUpdatesOnMain(_ updates: @escaping () -> Void) {
+        DispatchQueue.main.async {
+            updates()
+        }
+    }
+    
+    private func completeLogin() {
+        performUIUpdatesOnMain {
+            let controller = self.storyboard!.instantiateViewController(withIdentifier: "MapsViewController") as! UIViewController
+            self.present(controller, animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func login(_ sender: Any) {
+        
+        
+        let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"udacity\": {\"username\": \"\(email.text!)\", \"password\": \"\(password.text!)\"}}".data(using: String.Encoding.utf8)
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            guard (error == nil) else {
+                print("There was an error with your request: \(String(describing: error))")
+                return
+            }
+            let range = Range(5 ..< data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: newData!, options: .allowFragments) as! [String:AnyObject]
+            } catch {
+                print("Could not parse the data as JSON: '\(String(describing: newData))'")
+                return
+            }
+            
+//            for _ in parsedResult {
+//                guard let account = parsedResult["account"] as? [String:AnyObject] else {
+//                    print("Could not find account in \(parsedResult).")
+//                    return
+//                }
+//                
+//                for (key, value) in account {
+//                    if key == "registered" {
+//                        print(value)
+//                        self.appDelegate.userID = key
+//                        success = key as! Int
+//                        print(success)
+//                    } else {
+//                        print("mother ")
+//                    }
+//                }
+//                
+//            }
+        
+            guard let session = parsedResult["session"] as? [String:AnyObject] else {
+                print("Could not find session in \(String(describing: parsedResult["session"]))")
+                return
+            }
+            
+            guard let sessionId = session["id"] as? String else {
+                print("Could not find ID in \(String(describing: session["id"]))")
+                return
+            }
+            self.appDelegate.sessionID = sessionId
+            self.completeLogin()
+        }
+        task.resume()
     }
     
     @IBAction func signUp(_ sender: Any) {
