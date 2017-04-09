e28516893253a3e6a01ad3484572f3bec60eb205
Kiyoshi Woolheater <my email address used on my git repo>
2017-04-09 09:33:04 -0400

    stopped before making map annotation button

---
+++ OnTheMap/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  OnTheMap
+//
+//  Created by Kiyoshi Woolheater on 4/4/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    struct Keys {
+        let ApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    }
+    
+}
--- OnTheMap/MapsViewController.swift
+++ OnTheMap/MapsViewController.swift
@@ -11,12 +11,64 @@ import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
 
+    var appDelegate: AppDelegate!
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        loadStudentLocations()
+        
+            
+        
+        
     }
-
+    
+    func loadStudentLocations() {
+        let request = NSMutableURLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            guard (error == nil) else {
+                print("There was an error with your request: \(String(describing: error))")
+                return
+            }
+            
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:AnyObject]
+            } catch {
+                print("Could not parse the data as JSON: '\(String(describing: data))'")
+                return
+            }
+            
+            var results: [[String:AnyObject]]
+            for (_,value) in parsedResult {
+            
+                results = value as! [[String:AnyObject]]
+                
+                for student in results {
+                    
+                    if let latitude = student["latitude"], let longitude = student["longitude"], let firstName = student["firstName"],let lastName = student["lastName"], let mediaURL = student["mediaURL"] {
+                        let coord: MKPointAnnotation = MKPointAnnotation()
+                        coord.coordinate = CLLocationCoordinate2D(latitude: latitude as! CLLocationDegrees, longitude: longitude as! CLLocationDegrees)
+                        coord.title = ("\(firstName) \(lastName)")
+                        coord.subtitle = ("\(mediaURL)")
+                        self.mapView.addAnnotation(coord)
+                        
+                    } else {
+                        print("fail")
+                    }
+                }
+                
+            }
+            
+        }
+        task.resume()
+    }
+    
 }
 
