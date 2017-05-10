51d5a8ec20b1e8035785c07d69a454093ecfbe16
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-05-10 17:01:52 -0400

    load student locations moved to client class on table view

--- TableViewController.swift
+++ TableViewController.swift
@@ -41,57 +41,15 @@ class tableViewController: UITableViewController {
     }
     
     func loadStudentLocations() {
-        // fill array of people with data
-        let request = NSMutableURLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=200&skip=10&order=-updatedAt")!)
-        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-        let session = URLSession.shared
-        let task = session.dataTask(with: request as URLRequest) { data, response, error in
-            guard (error == nil) else {
-                print("There was an error with your request: \(String(describing: error))")
-                let alert = UIAlertController(title: "", message: "There was a network error with your request.", preferredStyle: UIAlertControllerStyle.alert)
-                alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                return
-            }
+        Client.sharedInstance().loadStudentLocations { (success, error) in
             
-            let parsedResult: [String:AnyObject]!
-            do {
-                parsedResult = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:AnyObject]
-            } catch {
-                print("Could not parse the data as JSON: '\(String(describing: data))'")
-                return
-            }
-            
-            var results: [[String:AnyObject]]
-            for (key ,value) in parsedResult {
-                
-                if key == "error" {
-                    let alert = UIAlertController(title: "", message: "There was a server error with your request.", preferredStyle: UIAlertControllerStyle.alert)
-                    alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                    return
-                } else {
-                
-                results = value as! [[String:AnyObject]]
-                
-                for student in results {
-                    
-                    self.person = people.personFromResults(results)
-                    
-//                    if let firstName = student["firstName"], let lastName = student["lastName"], let mediaURL = student["mediaURL"] {
-//                        
-//                        self.person.append(people(firstName: firstName as! String, lastName: lastName as! String, mediaURL: mediaURL as! String))
-//                        
-//                    }
+            if success {
+                self.performUIUpdatesOnMain {
+                    self.person = SavedItems.sharedInstance().array
+                    self.tableView.reloadData()
                 }
-                }
-            }
-            self.performUIUpdatesOnMain {
-                self.tableView.reloadData()
             }
         }
-        task.resume()
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
